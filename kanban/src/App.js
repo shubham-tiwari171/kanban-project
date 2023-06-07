d187fda796df59a6564ad1e14e2214ca14a8f8e6
Harshit Sachan <harshitsachan99@gmail.com>
2023-06-07 11:58:45 +0530

    Navbar done

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/atoms/Navbar/NavBar';
+import Card from './components/atoms/Card';
 
 function App() {
   return (
     <BrowserRouter>
+      <div className='App'>
+        <NavBar/>
+        <Card/>
+      </div>
       <Routes>
         <Route path='/' element={<div>Hello</div>}></Route>
       </Routes>
